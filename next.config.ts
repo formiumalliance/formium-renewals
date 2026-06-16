1c6c7ced7407bb7d9e9a371b4e81e246733f1bd4
formiumalliance <formiumalliance@gmail.com>
2026-06-16 14:29:17 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
